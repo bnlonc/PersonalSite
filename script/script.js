9a3d7e14a3a67bc0a5decfdc0d437fabff4204f7
Ben Lonc <bnlonc@gmail.com>
2023-09-04 16:48:22 -0400

    First commit

---
+++ script/script.js
@@ -0,0 +1,76 @@
+// Find out what the user's browser calls CSS transition-end events for browser compatibility. 
+// Code adapted from educative: https://www.educative.io/answers/how-to-detect-the-end-of-css-transition-events-in-javascript
+function getTransitionEndEventName() {
+    var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    }
+    
+    let bodyStyle = document.body.style;
+    
+    for(let transition in transitions) {
+        if(bodyStyle[transition] != undefined) {
+            return transitions[transition];
+        } 
+    }
+}
+
+let transitionEndEventName;
+const sliderSpeed = "0.75s";
+const homeBodyDocument = "html/home.html";
+let circleSlider;
+
+function onLoad() {
+    transitionEndEventName = getTransitionEndEventName();
+    circleSlider = document.getElementById("circleSlider");
+}
+
+function gotoPage(event, destination, buttonId) {
+
+    window.requestAnimationFrame(function(){
+        
+        circleSlider.style.transition = "0s";
+        circleSlider.style.background = getComputedStyle(document.getElementById(buttonId)).getPropertyValue("background");
+        circleSlider.style.left = event.clientX + "px";
+        circleSlider.style.top = event.clientY + "px";
+        
+        window.requestAnimationFrame(function(){
+
+            circleSlider.style.transition = sliderSpeed;
+    
+            radius = 2 * Math.sqrt(Math.pow(event.clientX, 2) + Math.pow(window.innerHeight - event.clientY, 2))
+
+            circleSlider.style.height = 1.2 * radius + "px";
+            circleSlider.style.width = 1.2 * radius + "px";
+        });
+    });
+
+    circleSlider.destination = destination;
+    circleSlider.buttonId = buttonId;
+    circleSlider.addEventListener(transitionEndEventName, onSliderCoverage);
+}
+
+function onSliderCoverage(event) {
+
+    circleSlider.removeEventListener(transitionEndEventName, onSliderCoverage);
+
+    document.getElementById("bodyFrame").src = event.currentTarget.destination;
+
+    buttonStyle = getComputedStyle(document.getElementById(event.currentTarget.buttonId));
+
+    document.getElementById("header").style.background = buttonStyle.getPropertyValue("background"); 
+    document.getElementById("headerFlash").style.color = buttonStyle.getPropertyValue("color"); 
+
+    window.requestAnimationFrame(function(){
+        circleSlider.style.height = "0px";
+        circleSlider.style.width = "0px";
+    });
+}
+
+addEventListener("beforeunload", function() {
+    
+    document.getElementById("bodyFrame").attr('src', homeBodyDocument);
+
+});
